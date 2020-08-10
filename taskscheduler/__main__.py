e0185169a818745c19722cbba3ee8b91ea019166
C1296230 Leandro de Souza Batista <c1296230@interno.bb.com.br>
2020-08-10 15:37:08 -0300

    flask OK

--- taskscheduler/__main__.py
+++ taskscheduler/__main__.py
@@ -1,8 +1,15 @@
+import os
 from flask import Flask
-from apis import Api
+from waitress import serve
 
 app = Flask(__name__)
 
-api.init_app(app)
 
-app.run(debug=True)
+def main():
+    port = int(os.environ.get("PORT", 5000))
+    serve(app, host='0.0.0.0', port=port,
+          threads=int(os.getenv("APP_THREADS", "1")))
+
+
+if __name__ == '__main__':
+    main()
