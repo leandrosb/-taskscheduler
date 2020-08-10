e00ee2a9b0e4c0a9a5fbfa4785c05fce32a48c34
C1296230 Leandro de Souza Batista <c1296230@interno.bb.com.br>
2020-08-10 15:37:37 -0300

    flask ok

--- taskscheduler/apis/__init__.py
+++ taskscheduler/apis/__init__.py
@@ -1,7 +1,1 @@
-from flask_restplus import Api
 
-api = Api(
-    title='ToDo',
-    version='0.1',
-    description='Task scheduler for me'
-)
--- taskscheduler/app.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from apis import api
-
-app = Flask
-api.init_app(app)
-
-app.run(debug=True)
